5073d643876c6d9c89586c0353791012e74dea55
KKAcTest <167745946+KKAcTest@users.noreply.github.com>
2024-04-22 09:13:58 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,134 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+# skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "main" {
+  name     = "KKwia-LinuxVM-RG"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "salinuxvm1"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "KKwiaLinuxVM-network"
+  address_space       = ["10.15.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.15.2.0/24"]
+}
+
+resource "azurerm_public_ip" "pubip" {
+  name                = "KKwiaLinuxVM-pubip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "KKwiaLinuxVM-nic1"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip.id
+  }
+}
+
+resource "azurerm_network_interface" "internal" {
+  name                = "KKwiaLinuxVM-nic2"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "webserver" {
+  name                = "tls_webserver"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  security_rule {
+    access                     = "Allow"
+    direction                  = "Inbound"
+    name                       = "tls"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "443"
+    destination_address_prefix = azurerm_network_interface.main.private_ip_address
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "main" {
+  network_interface_id      = azurerm_network_interface.internal.id
+  network_security_group_id = azurerm_network_security_group.webserver.id
+}
+
+
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "KKwiaLinuxVM"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_D2s_v3"
+  admin_username                  = var.Admin_username
+  admin_password                  = var.Admin_password
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+    azurerm_network_interface.internal.id,
+  ]
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(<<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y nginx
+    echo "<html><body><h1>Hello, world!</h1></body></html>" | sudo tee /var/www/html/index.html
+    sudo systemctl start nginx
+  EOF
+  )
+
+#  provisioner "remote-exec" {
+#  inline = [
+#    "sudo apt-get update",
+#    "sudo apt-get install -y nginx",
+#    "echo '<html><body><h1>Hello world</h1></body></html>' | sudo tee /var/www/html/index.html",
+#    "sudo systemctl start nginx",
+#    "sudo systemctl enable nginx",
+#    ]
+#  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "Admin_username" {
+  description = "VMLinux administrator username"
+  type        = string
+  sensitive   = true
+}
+
+variable "Admin_password" {
+  description = "VMLinux administrator password"
+  type        = string
+  sensitive   = true
+}
